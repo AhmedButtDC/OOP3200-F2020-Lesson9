3ab530632a073addc18e436387c1961cd8cccb0e
Ahmed Butt <TypicalNewb@outlook.com>
2020-11-11 10:40:10 -0500

    included basic input/output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -6,10 +6,21 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Declarations
         String name = null;
         Scanner keyboard = new Scanner(System.in);
+        float age = 0.0f;
+
+        //Input
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
-        System.out.printf("You entered: %s", name);
+
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+
+        //Output
+        System.out.println();
+        System.out.printf("You entered: %s \n", name);
+        System.out.printf("You entered: %.0f \n", age);
     }
 }
