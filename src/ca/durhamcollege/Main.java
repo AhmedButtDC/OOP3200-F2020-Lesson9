e315edd31cff5e7a65b4e2e43596d73bb12bdfa7
Ahmed Butt <TypicalNewb@outlook.com>
2020-11-11 11:32:47 -0500

    loopy

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,5 +1,6 @@
 package ca.durhamcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -15,8 +16,28 @@ public class Main
         System.out.print("Please enter your name: ");
         name = keyboard.nextLine();
 
-        System.out.print("Please enter your age: ");
-        age = keyboard.nextFloat();
+        boolean isValid = false;
+
+        while (!isValid)
+        {
+            try
+            {
+                System.out.print("Please enter your age: ");
+                age = keyboard.nextFloat();
+                isValid = true;
+            }
+            catch (Exception e)
+            {
+                System.out.printf("Error: You must enter a valid floating-point number.\n");
+                isValid = false;
+                keyboard.nextLine();
+            }
+            if (age < 0.0f || age > 120.0f)
+            {
+                System.out.printf("Error: You must enter an age greater than 0 and less than 120.\n");
+                isValid = false;
+            }
+        }
 
         //Output
         System.out.println();
