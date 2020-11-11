3b9416607e7d65724784f6b796c66bd8941de0bb
Ahmed Butt <TypicalNewb@outlook.com>
2020-11-11 10:32:18 -0500

    included basic input/output

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,10 +1,15 @@
 package ca.durhamcollege;
 
+import java.util.Scanner;
+
 public class Main
 {
-
     public static void main(String[] args)
     {
-
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s", name);
     }
 }
